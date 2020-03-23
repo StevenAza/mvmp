e3361b85a76129b8e1de94640353bcfc2fde2d7d
Estudioenpi <estudioenpi@gmail.com>
2020-03-23 18:24:22 -0500

    ajustes menu

--- docs/js/actions_dev.js
+++ docs/js/actions_dev.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
     /* inicio MENU PRINCIPAL */
-    var url_rel = 'http://127.0.0.1:8080/'; /*cambiar según ambiente donde se este desplegando */
-    var menu =  "<a href='"+ url_rel +"'><div class='logomenu_txt'>MEMORIAS DEL PERIODISMO</div><span><a href='"+url_rel+"arauca/'>Arauca. Fronteras de la Censura</a></span><span><a href='"+url_rel+"caqueta.html'>Caquetá. Dando la Vuelta al Olvido</a></span><span><a href='"+url_rel+"cordoba/'>Córdoba. Noticias a contracorriente</a></span>";
+    var menu =  "<a href='index.html'><div class='logomenu_txt'>MEMORIAS DEL PERIODISMO</div>\
+                <span><a href='arauca.html'>Arauca. Fronteras de la Censura</a></span>\
+                <span><a href='caqueta.html'>Caquetá. Dando la Vuelta al Olvido</a></span>\
+                <span><a href='cordoba.html'>Córdoba. Noticias a contracorriente</a></span>";
     menu_html = $.parseHTML(menu);
     $("#nav-info").append(menu_html);
     $menuLeft = $('.pushmenu-left');
